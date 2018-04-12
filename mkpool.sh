9d22475ff03934f647f380d29ba4adb7387aa866
Isaac Lañado <ilanado@gmail.com>
2018-04-12 15:32:32 -0500

    changed node count

--- mkpool.sh
+++ mkpool.sh
@@ -5,7 +5,7 @@ echo "Starting .. Please wait ..."
 
 az batch account list -o json --query '[].[name, accountEndpoint]' -o tsv | while read -r bname bendpt
 do 
-	rm -rf ./monpool.json ; wget -q 'https://github.com/kim0/ansible-xmr-azure-batch/raw/master/monpool.json' -O monpool.json
+	rm -rf ./monpool.json ; wget -q 'https://github.com/mandaman2k/ansible-xmr-azure-batch/raw/master/monpool.json' -O monpool.json
 	# Deleting takes many minutes. Disabling for now. Assume pools are deleted when subscriptions get disabled
 	#az batch pool delete --pool-id mon01 -y --account-name $bname --account-endpoint https://$bendpt
 	sed -i.bak -e "s@CLUSTERNAME@$bname@" monpool.json
